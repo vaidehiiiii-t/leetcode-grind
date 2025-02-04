743915910c82d50db387cf0cb55c600baface8b1
Aditi Trivedi <adititrivedi2017@gmail.com>
2025-02-04 20:37:56 +0530

    1800. Maximum Ascending Subarray Sum 2nd approach

--- January 2025/1800MaximumAscendingSubarraySum.cpp
+++ January 2025/1800MaximumAscendingSubarraySum.cpp
@@ -9,3 +9,23 @@ public:
         return ans;
     }
 };
+
+//2nd approach 
+class Solution {
+public:
+    int maxAscendingSum(vector<int>& nums) {
+        int currentSum = nums[0], maxSum = currentSum;
+        
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] > nums[i - 1]) {
+                currentSum += nums[i]; 
+            } else {
+                currentSum = nums[i];
+            }
+            maxSum = max(maxSum, currentSum); 
+        
+        }
+        
+        return maxSum;
+    }
+};
