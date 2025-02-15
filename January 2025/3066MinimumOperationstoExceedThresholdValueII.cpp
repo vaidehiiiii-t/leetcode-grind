28df278ed3eff860e50e39ade691233d539243ac
Aditi Trivedi <adititrivedi2017@gmail.com>
2025-02-15 23:35:05 +0530

    2698. Find the Punishment Number of an Integer

--- January 2025/3066MinimumOperationstoExceedThresholdValueII.cpp
+++ January 2025/3066MinimumOperationstoExceedThresholdValueII.cpp
@@ -22,4 +22,29 @@ class Solution {
     }();
 
 
-    
+    //2nd approach
+
+    class Solution {
+        public:
+            static int minOperations(vector<int>& nums, int k) {
+                nums.push_back(k);
+                priority_queue<int, vector<int>, greater<>> pq(nums.begin(), nums.end());
+                int ans=0;
+                while(pq.top()<k && pq.size()>=2){
+                    int x=pq.top(); pq.pop();
+                    int y=pq.top(); pq.pop();
+                    if (2LL*x+y<k)
+                        pq.push(2*x+y);
+                    ans++;
+                }
+                return ans;  
+            }
+        };
+        
+        
+        auto init = []() {
+            ios::sync_with_stdio(0);
+            cin.tie(0);
+            cout.tie(0);
+            return 'c';
+        }();
