cd5a05ced58c254466678811e7602269b0198c59
Aditi Trivedi <adititrivedi2017@gmail.com>
2025-02-25 22:11:14 +0530

    1524. Number of Sub-arrays With Odd Sum Approach for Counting Subarrays with Odd Sum

--- 1524NumberofSubarraysWithOddSum.cpp
+++ 1524NumberofSubarraysWithOddSum.cpp
@@ -21,4 +21,23 @@ class Solution {
         return 'c';
     }();
 
+
+    //Approach for Counting Subarrays with Odd Sum
     
+    class Solution {
+        public:
+            int numOfSubarrays(vector<int>& arr) {
+                const int MOD=1e9+7;
+                long long o=0,e=1,s=0,r=0;
+                for(int a:arr){
+                    s+=a;
+                    if(s%2==0){
+                        r=(r+o)%MOD;
+                        e++;
+                    }else{
+                        r=(r+e)%MOD;
+                        o++;
+                    }
+                }return r;
+            }
+        };
