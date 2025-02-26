99602748f2bfc0ce5ae1d2ebe58758af64a0b251
Aditi Trivedi <adititrivedi2017@gmail.com>
2025-02-26 21:05:12 +0530

    1749. Maximum Absolute Sum of Any Subarray prefix sum method

--- 1749MaximumAbsoluteSumofAnySubarray.cpp
+++ 1749MaximumAbsoluteSumofAnySubarray.cpp
@@ -10,3 +10,29 @@ class Solution {
             return abs(maxSum - minSum);
         }
     };
+
+    //prefix sum method
+    class Solution {
+        public:
+            int maxAbsoluteSum(std::vector<int>& nums) {
+                int prefixSum = 0;
+                int minPrefixSum = 0;
+                int maxPrefixSum = 0;
+                int maxAbsoluteSum = 0;
+        
+                for (int i = 0; i < nums.size(); i++) {
+                    prefixSum += nums[i]; // Update prefix sum
+                    
+                    // Update maxAbsoluteSum
+                    maxAbsoluteSum = std::max(maxAbsoluteSum, std::abs(prefixSum - minPrefixSum));
+                    maxAbsoluteSum = std::max(maxAbsoluteSum, std::abs(prefixSum - maxPrefixSum));
+                    
+                    // Update minPrefixSum and maxPrefixSum
+                    minPrefixSum = std::min(minPrefixSum, prefixSum);
+                    maxPrefixSum = std::max(maxPrefixSum, prefixSum);
+                }
+        
+                return maxAbsoluteSum;
+            }
+        };
+        
